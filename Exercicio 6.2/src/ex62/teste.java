d36af3129ca6b82d9a232dea24dbf980d13b6ef0
Anderson Ribeiro <62036560+WabDaveloper@users.noreply.github.com>
2020-04-04 17:53:53 -0300

    Exercícios finalizados
    Exercícios finalizados

---
+++ Exercicio 6.2/src/ex62/teste.java
@@ -0,0 +1,13 @@
+package ex62;
+
+public class teste {
+	
+	
+	public static void main(String[] args) {
+		int [] vetor = {18,8,65,789,10,165,30,123,5,3,2,13};
+		QuickSort q = new QuickSort();
+		q.ordenar(vetor);
+
+	}
+
+}
